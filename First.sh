1ab1933f178691a91e0700a382ed42f3dea30430
Dineshdini71 <dinidinesh71@gmail.com>
2024-06-06 19:47:54 +0530

    New commit

--- First.sh
+++ First.sh
@@ -7,3 +7,34 @@ C="This is Dinesh Practicing shellScrpiting"
 echo $A
 echo ${B}
 echo "${c}"
+
+
+# This is our first script
+
+#!/bin/bash
+rm -rf *.zip
+T_version=1.8.5
+P_version=1.11.0
+
+
+echo "Let's download teraform version $T_version"
+wget https://releases.hashicorp.com/terraform/${T_version}/terraform_${T_version}_linux_amd64.zip
+
+ls -al
+echo "Succeesfully downloaded the Terraform $T_version"
+
+unzip terraform_${T_version}_linux_amd64.zip && rm -rf terraform_${T_version}_linux_amd64.zip
+
+echo "************* END *************"
+
+echo " Let's download the packer version $P_version"
+wget https://releases.hashicorp.com/packer/${p_version}/packer_${P_version}_linux_amd64.zip
+
+ls -al
+echo "Succeesfully downloaded the Packer $P_version"
+
+unzip terraform_${p_version}_linux_amd64.zip && rm -rf terraform_${p_version}_linux_amd64.zip
+
+echo "************* END *************"
+
+ls -al
